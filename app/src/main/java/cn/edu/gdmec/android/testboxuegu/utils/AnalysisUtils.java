8877b0f2c72a7fc3e7c6963bfdf94ef7d81f6048
killer-L <2304806342@qq.com>
2017-12-25 15:34:47 +0800

    2.3 ok

---
+++ app/src/main/java/cn/edu/gdmec/android/testboxuegu/utils/AnalysisUtils.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.testboxuegu.utils;
+
+/**
+ * Created by student on 17/12/25.
+ */
+
+public class AnalysisUtils {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/testboxuegu/view/ExerciseView.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.testboxuegu.view;
+
+/**
+ * Created by student on 17/12/25.
+ */
+
+public class ExerciseView {
+}
